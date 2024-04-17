ae73c7f4627666a22c67b31d89c5871b9f591ffe
abizy nelies <abizynelies9@gmai.com>
2024-04-17 10:29:31 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+for(let i=0; i<document.querySelectorAll(".drum").length; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+       var buttonClick = this.innerHTML
+       MakeSound(buttonClick) 
+       animate(buttonClick)  
+    });
+}
+
+document.addEventListener("keypress",function (event) {
+    var keypressed=event.key
+    MakeSound(keypressed)
+    animate(keypressed)
+})
+
+function MakeSound(key) {
+
+    switch (key) {
+        case "w":
+           var audio = new Audio("sounds/tom-1.mp3")
+           audio.play()  
+            break;
+
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3")
+                audio.play()  
+                 break;
+
+                 case "s":
+                var audio = new Audio("sounds/tom-3.mp3")
+                audio.play()  
+                 break;
+
+                 case "d":
+                var audio = new Audio("sounds/tom-4.mp3")
+                audio.play()  
+                 break;
+
+                 case "j":
+                    var audio = new Audio("sounds/crash.mp3")
+                    audio.play()  
+                     break;
+
+                     case "k":
+                        var audio = new Audio("sounds/kick-bass.mp3")
+                        audio.play()  
+                         break;
+                          
+                         case "l":
+                            var audio = new Audio("sounds/snare.mp3")
+                            audio.play()  
+                             break;
+               
+      
+        default: console.log(buttonClick)
+            break;
+      }
+}
+function animate(keyflash) {
+   var ani = document.querySelector("."+keyflash)
+       ani.classList.add("pressed")
+}
